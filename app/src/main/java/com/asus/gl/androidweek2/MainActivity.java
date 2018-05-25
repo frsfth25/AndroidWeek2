02db71778cd91573d967d71a73200778fbfef9c0
Faris Fathurrahman <farisfathurrahman.25@gmail.com>
2018-05-25 14:54:52 +0300

    Enhancement in Code
    - main layout
    - main activity

--- app/src/main/java/com/asus/gl/androidweek2/MainActivity.java
+++ app/src/main/java/com/asus/gl/androidweek2/MainActivity.java
@@ -2,49 +2,47 @@ package com.asus.gl.androidweek2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import bsh.Interpreter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     private TextView t_res;
     private String operation_text = "";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         t_res = findViewById(R.id.txt_res);
         t_res.setText(operation_text);
-
     }
 
-    public void btnClick(View v){
+    public void btnClick(View v)
+    {
         Button b = (Button)v;
         String val = b.getText().toString();
         operation_text +=  val;
         t_res.setText(operation_text);
-
-
     }
 
-    public void btnEqual(View view) {
+    public void btnEqual(View view)
+    {
         Interpreter interpreter = new Interpreter();
-        try {
+        try
+        {
             interpreter.eval("result = "+ operation_text);
             t_res.setText(interpreter.get("result").toString());
             operation_text="";
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             t_res.setText("Syntax error!");
             operation_text="";
-
         }
-
     }
 }
